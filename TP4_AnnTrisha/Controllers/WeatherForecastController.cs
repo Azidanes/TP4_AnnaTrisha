24f31be651a920e5a2ee3fcadeb459dc7e20d5e1
AZ <annazidanes@gmail.com>
2023-08-24 12:35:42 -0400

    2 Get, 1 Post

--- TP4_AnnTrisha/Controllers/WeatherForecastController.cs
+++ TP4_AnnTrisha/Controllers/WeatherForecastController.cs
@@ -1,15 +1,18 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
 
 namespace TP4_AnnTrisha.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("[controller]/[action]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
+        private static string[] Summaries = new[]
         {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
+
+        List<string> values = new List<string> {"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching" };
 
         private readonly ILogger<WeatherForecastController> _logger;
 
@@ -19,33 +22,54 @@ namespace TP4_AnnTrisha.Controllers
         }
 
         [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
+        public IEnumerable<WeatherForecast> Get(int day)
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
-                Date = DateTime.Now.AddDays(index),
+                Date = DateTime.Now.AddDays(day),
                 TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                Summary = values[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+
+        }
+        [HttpGet(Name = "GetWeatherForecast2")]
+        public IEnumerable<WeatherForecast> Get2()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index + 5),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = values[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
         }
+
+        [HttpPost]
+        public IEnumerable<string> AjouterValeur(string strValeur)
+        {
+            values.Add(strValeur);
+            return values;
+        }
+
+
 
         //hello
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
+        //public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        //{
+        //    _logger = logger;
+        //}
 
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
-        }
+        //[HttpGet(Name = "GetWeatherForecast")]
+        //public IEnumerable<WeatherForecast> Get()
+        //{
+        //    return Enumerable.Range(1, 3).Select(index => new WeatherForecast
+        //    {
+        //        Date = DateTime.Now.AddDays(index),
+        //        TemperatureC = Random.Shared.Next(-20, 55),
+        //        Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        //    })
+        //    .ToArray();
+        //}
     }
 }
