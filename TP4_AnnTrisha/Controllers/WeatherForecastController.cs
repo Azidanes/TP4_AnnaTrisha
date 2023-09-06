89f1c218f06c0fe596c2defe393edbf267771fe4
wind-takes-them <134608206+wind-takes-them@users.noreply.github.com>
2023-09-06 17:09:43 -0400

    api customization

--- TP4_AnnTrisha/Controllers/WeatherForecastController.cs
+++ TP4_AnnTrisha/Controllers/WeatherForecastController.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Xml.Linq;
+using System;
+using System.IO;
 
 namespace TP4_AnnTrisha.Controllers
 {
@@ -7,69 +9,198 @@ namespace TP4_AnnTrisha.Controllers
     [Route("[controller]/[action]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static string[] Summaries = new[]
-        {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
 
-        List<string> values = new List<string> {"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching" };
+
+        string[,] chansons = { { "Cradle", "Exits", "What You Know" }, { "Direction", "Moth", "Dirty" }, { "99 Glooms", "Catalysta", "Lost It To Trying" } };
+        string[,] artistes = { { "Silversun Pickups", "Foals", "Two Door Cinema Club" }, { "Solence", "Normandie", "grandson" }, { "Sta", "Pkch", "Son Lux" } };
+
+        string docPath =
+  Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+        int i = 3;
+
+        List<string> CustomNames = new List<string>();
+        List<string> CustomArtists = new List<string>();
+        List<string> CustomGenres = new List<string>();
 
         private readonly ILogger<WeatherForecastController> _logger;
-
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get(int day)
+
+
+        [HttpGet(Name = "AvoirChansonsAleotoires")]
+        public List<Song> AvoirChansonsAleatoires(int NombreDeChansons)
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            var SongList = new List<Song>();
+
+            for (int i = 0; i < NombreDeChansons; i++)
             {
-                Date = DateTime.Now.AddDays(day),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = values[Random.Shared.Next(Summaries.Length)]
+                int g = Random.Shared.Next(0, 3);
+                int n = Random.Shared.Next(0, 3);
+                string style;
+
+
+
+                switch (g)
+                {
+                    case 0:
+                        style = "Chill"; break;
+                    case 1:
+                        style = "Heavy"; break;
+                    case 2:
+                        style = "Electronic"; break;
+                    default:
+                        style = string.Empty; break;
+                }
+
+
+
+                SongList.Add(new Song
+                {
+                    Artist = artistes[g, n],
+                    Name = chansons[g, n],
+                    Style = style,
+
+                });
+            }
+            return SongList;
+        }
+
+
+
+        [HttpGet(Name = "AvoirChansonDuGenre")]
+        public IEnumerable<Song> AvoirChansonDuGenre(string genre)
+        {
+            
+            switch (genre.ToLower())
+            {
+                case "chill": 
+                    i = 0; break;
+                case "heavy":
+                    i = 1; break;
+                case "electronic":
+                    i = 2; break;
+                default:
+                    i = 3; break;
+            }
+
+            int ii = Random.Shared.Next(0, 3);
+
+            //dans le cas ou le genre entree est invalide
+            if (i == 3)
+            {
+                return Enumerable.Range(1, 1).Select(index => new Song
+                {
+                    Artist = "invalide",
+                    Name = "invalide",
+                    Style = "Choisir parmis les genres suivants: chill, heavy, electronic"
+                })
+                .ToArray();
+            }
+
+            //retourner une chanson
+            return Enumerable.Range(1, 1).Select(index => new Song
+            {
+                Artist = artistes[i, ii],
+                Name = chansons[i, ii],
+                Style = genre
             })
             .ToArray();
 
         }
-        [HttpGet(Name = "GetWeatherForecast2")]
-        public IEnumerable<WeatherForecast> Get2()
+
+
+        [HttpPost(Name = "AjouterChanson")]
+        public Song AjouterChanson(string Titre, string Artiste, string Genre)
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+
+
+            CustomNames.Add(Titre);
+            CustomArtists.Add(Artiste);
+            CustomGenres.Add(Genre);
+            
+
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, "CustomNames.txt"), true))
             {
-                Date = DateTime.Now.AddDays(index + 5),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = values[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+                foreach (string line in CustomNames)
+                    outputFile.WriteLine(line);
+            }
+
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, "CustomArtists.txt"), true))
+            {
+                foreach (string line in CustomArtists)
+                    outputFile.WriteLine(line);
+            }
+
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, "CustomGenres.txt"), true))
+            {
+                foreach (string line in CustomGenres)
+                    outputFile.WriteLine(line);
+            }
+
+            return new Song
+            {
+                Artist = Artiste,
+                Name = Titre,
+                Style= Genre
+            };
         }
 
-        [HttpPost]
-        public IEnumerable<string> AjouterValeur(string strValeur)
+
+
+
+
+
+
+
+        [HttpGet(Name = "AvoirChansonsAjoutees")]
+        public List<Song> AvoirChansonsAjoutees()
         {
-            values.Add(strValeur);
-            return values;
+            var SongList = new List<Song>();
+
+                using (var sr = new StreamReader(Path.Combine(docPath, "CustomNames.txt")))
+                {
+                for (string fullLine = sr.ReadLine(); fullLine != null; fullLine = sr.ReadLine())
+                    {
+                        string[] wholeLine = fullLine.Split('\t');
+                        CustomNames.Add(wholeLine[0]);
+                    }
+                }
+                using (var sr = new StreamReader(Path.Combine(docPath, "CustomArtists.txt")))
+                {
+                    for (string fullLine = sr.ReadLine(); fullLine != null; fullLine = sr.ReadLine())
+                    {
+                        string[] wholeLine = fullLine.Split('\t');
+                        CustomArtists.Add(wholeLine[0]);
+                    }
+                }
+                using (var sr = new StreamReader(Path.Combine(docPath, "CustomGenres.txt")))
+                {
+                    for (string fullLine = sr.ReadLine(); fullLine != null; fullLine = sr.ReadLine())
+                    {
+                        string[] wholeLine = fullLine.Split('\t');
+                        CustomGenres.Add(wholeLine[0]);
+                    }
+                }
+
+            if (CustomNames.Count > 0)
+            {
+                for (int i = 0; i < CustomNames.Count; i++)
+                {
+
+                    SongList.Add(new Song
+                    {
+                        Artist = CustomArtists[i],
+                        Name = CustomNames[i],
+                        Style = CustomGenres[i],
+
+                    });
+                }
+            }
+            return SongList;
         }
-
-
-
-        //hello
-        //public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        //{
-        //    _logger = logger;
-        //}
-
-        //[HttpGet(Name = "GetWeatherForecast")]
-        //public IEnumerable<WeatherForecast> Get()
-        //{
-        //    return Enumerable.Range(1, 3).Select(index => new WeatherForecast
-        //    {
-        //        Date = DateTime.Now.AddDays(index),
-        //        TemperatureC = Random.Shared.Next(-20, 55),
-        //        Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        //    })
-        //    .ToArray();
-        //}
     }
 }
---
+++ TP4_AnnTrisha/Song.cs
@@ -0,0 +1,11 @@
+﻿namespace TP4_AnnTrisha
+{
+    public class Song
+    {
+        public string? Name { get; set; }
+
+        public string? Artist { get; set; }
+
+        public string? Style { get; set; }
+    }
+}
